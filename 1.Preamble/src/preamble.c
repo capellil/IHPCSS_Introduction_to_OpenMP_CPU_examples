2e6d5a6fdfd3301d7147825d37608466f0ee4561
Ludovic Capelli <capellil.ludovic@gmail.com>
2023-06-09 05:39:20 -0400

    Initial commit: preamble example.
    The preamble example contains basic source code to check whether the compilation process is working or if modules are missing.

---
+++ 1.Preamble/src/preamble.c
@@ -0,0 +1,31 @@
+/**
+ * @file preamble.c
+ * @brief This example is to check that the compilation process works.
+ * @details You will find two folders in this example, and will in all examples
+ * provided in this session:
+ * - src: contains the source code.
+ * - bin: contains the binary produced.
+ *
+ * The makefile provided already sets everything up for you:
+ * - To compile it, you just need to issue the command `make`.
+ * - To run it, simply issue the command `./bin/preamble`.
+ *
+ * If you have any questions, do not hesitate.
+ * @author Ludovic Capelli
+ * @email l.capelli@epcc.ed.ac.uk
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+/**
+ * @brief Just prints a hello world message.
+ **/
+int main()
+{
+	int num_threads = omp_get_num_threads();
+	printf("Hello world, there are %d threads (i.e.: I am alone... T_T).\n", num_threads);
+
+	return EXIT_SUCCESS;
+}
