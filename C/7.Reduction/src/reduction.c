1443a169bf9097f418c08f9755a9351d86dae160
Capelli Ludovic <l.capelli@ed.ac.uk>
2023-06-25 11:50:05 +0100

    Reduction example moved to increment one.

--- C/7.Reduction/src/reduction.c
+++ C/7.Reduction/src/reduction.c
@@ -26,33 +26,26 @@
  **/
 int main()
 {
-	int* my_array = NULL;
-	int sum = 0;
-	int min = 0;
-	int max = 0;
-
-	#pragma omp parallel default(none) shared(my_array, sum, min, max)
+	int thread_count = 0;
+	#pragma omp parallel default(none) shared(thread_count)
 	{
-		#pragma omp master
-		{
-			my_array = (int*)malloc(sizeof(int) * omp_get_num_threads());
-			min = omp_get_num_threads() - 1;
-		}
+		#pragma omp atomic
+		thread_count++;
+
 		#pragma omp barrier
-		my_array[omp_get_thread_num()] = omp_get_thread_num();
-		#pragma omp barrier
-		#pragma omp for reduction(+: sum) reduction(min: min) reduction(max: max)
-		for(int i = 0; i < omp_get_num_threads(); i++)
-		{
-			sum += my_array[i];
-			min = (min > my_array[i]) ? my_array[i] : min;
-			max = (max < my_array[i]) ? my_array[i] : max;
-		}
 		#pragma omp single
 		{
-			printf("Sum = %d, min = %d, max = %d\n", sum, min, max);
+			// Restriction: the print statement must be kept inside the parallel region.
+			printf("There are %d threads.\n", thread_count);
 		}
 	}
+
+    thread_count = 0;
+	#pragma omp parallel default(none) reduction(+:thread_count)
+	{
+		thread_count++;
+	}
+	printf("There are %d threads.\n", thread_count);
 
 	return EXIT_SUCCESS;
 }
